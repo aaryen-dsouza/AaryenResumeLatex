26fec656a255e58a2b788b47e9b20077eb7ac4c2
aaryen-dsouza <dc.aaryen@gmail.com>
2026-03-02 22:24:43 -0500

    Add professional summary section and refine technical skills in resume

--- DA/copy.bat
+++ DA/copy.bat
@@ -3,7 +3,7 @@ REM filepath: /d:/Studies/Masters/CO-OP/AaryenResume/SD/copy_pdf.bat
 set SOURCE_DIR=d:\Canada\UoW\COOP\AaryenResumeLatex\DA
 set DEST_DIR=d:\Canada\JobHunt\FullTime\Current
 set SRC_PDF_NAME=main.pdf
-set DES_PDF_NAME=AaryenDSouza_DAResume.pdf
+set DES_PDF_NAME=AaryenDSouza_Resume_DA.pdf
 
 echo Copying %SRC_PDF_NAME% from %SOURCE_DIR% to %DEST_DIR%
 copy "%SOURCE_DIR%\%SRC_PDF_NAME%" "%DEST_DIR%\%DES_PDF_NAME%"
--- SD/copy.bat
+++ SD/copy.bat
@@ -3,7 +3,7 @@ REM filepath: /d:/Studies/Masters/CO-OP/AaryenResume/SD/copy_pdf.bat
 set SOURCE_DIR=d:\Canada\UoW\COOP\AaryenResumeLatex\SD
 set DEST_DIR=d:\Canada\JobHunt\FullTime\Current
 set SRC_PDF_NAME=main.pdf
-set DES_PDF_NAME=AaryenDSouza_SDResume.pdf
+set DES_PDF_NAME=AaryenDSouza_Resume_SD.pdf
 
 echo Copying %SRC_PDF_NAME% from %SOURCE_DIR% to %DEST_DIR%
 copy "%SOURCE_DIR%\%SRC_PDF_NAME%" "%DEST_DIR%\%DES_PDF_NAME%"
